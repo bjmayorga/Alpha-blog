bf9a23d9695f4b0724691f51cd15e238b7695441
Rails Udemy Course <bryanjmayorga@outlook.com>
2018-02-27 17:19:10 -0500

    Install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#home'
+  root 'welcome#alphablog'
   get 'about', to: 'welcome#about'
   resources :articles
   # Example of regular route:
